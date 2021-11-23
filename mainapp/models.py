f9696104754e80271771fa0e1236b7f9abfadf30
Wazzapio <myster1on.toks@gmail.com>
2021-11-23 20:09:25 +0300

    did work with models

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Product(models.Model):
     name = models.CharField(max_length=128)
     image = models.ImageField(upload_to='product_image', blank=True)
@@ -12,3 +15,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
