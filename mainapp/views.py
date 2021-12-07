6196acedced4973d7e6fb10a63d0016046151117
Wazzapio <myster1on.toks@gmail.com>
2021-12-07 16:13:13 +0300

    fixed detail

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 from mainapp.models import ProductCategory, Product
+
 
 # Create your views here.
 
@@ -20,9 +23,19 @@ def products(request):
 
     return render(request, 'mainapp/products.html', context)
 
-def detail(request, id):
-    context = {
-        'product': Product.objects.get(id=id)
-    }
+# def detail(request, id):
+#     context = {
+#         'product': Product.objects.get(id=id)
+#     }
+#
+#     return render(request, 'mainapp/detail.html', context)
 
-    return render(request, 'mainapp/detail.html', context)
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        return context
