18e59a218e5cbe3a17faf3a05ff32b79c98aeb5a
Wazzapio <myster1on.toks@gmail.com>
2021-12-20 22:13:52 +0300

    save profile

--- authapp/forms.py
+++ authapp/forms.py
@@ -4,12 +4,11 @@ import random
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 
-from authapp.models import User
+from authapp.models import User, UserProfile
 from authapp.validator import validate_first_name, validate_last_name
 
 
 class UserLoginForm(AuthenticationForm):
-
     # username = forms.CharField(widget=forms.TextInput(), validators=[validate_name])
 
     class Meta:
@@ -51,7 +50,7 @@ class UserRegisterForm(UserCreationForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
 
-    def save(self,commit=True):
+    def save(self, commit=True):
         user = super(UserRegisterForm, self).save()
         user.is_active = False
         salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
@@ -78,3 +77,18 @@ class UserProfileForm(UserChangeForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileEditForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if field_name != 'gender':
+                field.widget.attrs['class'] = 'form-control py-4'
+            else:
+                field.widget.attrs['class'] = 'form-control'
