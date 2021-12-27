d80671e8d0295aaf4904ecb3bcca6fff4fbb282e
Wazzapio <myster1on.toks@gmail.com>
2021-12-27 19:09:02 +0300

    quantity

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,4 +1,6 @@
 from django.db import transaction
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
@@ -26,7 +28,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
     fields = []
     success_url = reverse_lazy('orders:list')
     title = 'GeekShop | Создание заказа'
-    
+
     def get_context_data(self, **kwargs):
         context = super(OrderCreate, self).get_context_data(**kwargs)
 
@@ -42,7 +44,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                # basket_item.delete()
+                basket_item.delete()
             else:
                 formset = OrderFormSet()
         context['orderitems'] = formset
@@ -114,3 +116,33 @@ def order_forming_complete(request, pk):
     order.status = Order.SEND_TO_PROCEED
     order.save()
     return HttpResponseRedirect(reverse('orders:list'))
+
+
+@receiver(pre_save, sender=Basket)
+@receiver(pre_save, sender=OrderItem)
+def product_quantity_update_save(sender, instance, **kwargs):
+    if instance.pk:
+        instance.product.quantity -= instance.quantity - instance.get_item(int(instance.pk))
+    else:
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
+
+
+@receiver(pre_delete, sender=Basket)
+@receiver(pre_delete, sender=OrderItem)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.save()
+
+    # def delete(self, *args, **kwargs):
+    #     self.product.quantity += self.quantity
+    #     self.save()
+    #     super(Basket, self).delete(*args, **kwargs)
+    #
+    # def save(self, *args, **kwargs):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - self.get_item(int(self.pk))
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(Basket, self).save(*args, **kwargs)
