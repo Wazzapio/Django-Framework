677fe24242c98914bde3c4a3d45c37257ece1c0b
Wazzapio <myster1on.toks@gmail.com>
2022-01-19 02:11:16 +0300

    lesson_7 cache middleware

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.cache.UpdateCacheMiddleware'
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
